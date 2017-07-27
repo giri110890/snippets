e1352cbb66a198714b634c40907bdf583bccf1a7
PUNNAMARAJU.V.R. GIRIDHAR <punnamaraju.v.r.giridhar@ericsson.com>
2017-07-27 16:31:31 +0530

    testing the git

---
+++ test.py
@@ -0,0 +1,11 @@
+'''
+from datetime import datetime, timedelta
+
+s = '01.07.2017 23:45'
+date = datetime.strptime(s, "%d.%m.%Y %H:%M")
+for i in range(96):
+    date = date + timedelta(minutes=15)
+    final = datetime.strftime(date, "%d.%m.%Y %H:%M")
+    print((final))
+'''
+print(2**16)
